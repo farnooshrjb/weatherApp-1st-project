49596b97f40c8360da5efa0dd570db34298460d3
Farnoosh Rajabi <farnoosh.rajabi14@gmail.com>
2023-03-09 19:35:35 +0330

    display temprature , wind , humidity and... by using API in JavaScript

--- index.js
+++ index.js
@@ -23,3 +23,26 @@ function diaplayTemprature(response) {
     let currentDate = document.querySelector("#current-date");
     currentDate.innerHTML = formatDate(response.data.dt * 1000)
 }
+// display temprature , wind , humidity and... by using API IN JavaScript:
+
+function displayTemprature(response) {
+    console.log(response.data);
+    let tempratureElement = document.querySelector("#temperature");
+    tempratureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+
+
+
+let apiKey = `ab82911f5489acd5368aba2902d18e38`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london &appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemprature);
