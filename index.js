66c172b8dcff1f5c2273bb15ffc8516a9c39193f
Farnoosh Rajabi <farnoosh.rajabi14@gmail.com>
2023-03-09 22:11:57 +0330

    update JavaScrip for time & date

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 // In your project, display the current date and time using JavaScript
 
+
 function formatDate(timestamp) {
     let date = new Date(timestamp);
 
-    let date = now.getDate();
+
 
     let days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
     let day = days[date.getDay()];
 
-    let hours = now.getHours();
+    let hours = date.getHours();
     if (hours < 10) {
         hours = `0${hours}`;
     }
-    let minutes = now.getMinutes();
+    let minutes = date.getMinutes();
     if (minutes < 10) {
         minutes = `0${minutes}`;
     }
     return `${day} ${hours}:${minutes}`;
 }
 
-function diaplayTemprature(response) {
-    let currentDate = document.querySelector("#current-date");
-    currentDate.innerHTML = formatDate(response.data.dt * 1000)
 }
 // display temprature , wind , humidity and... by using API IN JavaScript:
 
@@ -37,12 +35,17 @@ function displayTemprature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    //Time & Date:
+    let dateElement = document.querySelector("#current-date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000)
+}
+
 }
 
 
 
 
-
+let city = "paris";
 let apiKey = `ab82911f5489acd5368aba2902d18e38`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london &appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city} &appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemprature);
